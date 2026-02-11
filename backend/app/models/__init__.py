b2cb542690a180a8749fd7ae9d4f4f90d8223e27
jhap <jhapgutz09@gmail.com>
2026-02-11 16:35:41 +0800

    department added

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,4 +1,4 @@
 from .item import Item
-from .user import User, UserRole
+from .user import User, UserRole, DepartmentType
 
-__all__ = ["Item", "User", "UserRole"]
+__all__ = ["Item", "User", "UserRole", "DepartmentType"]
